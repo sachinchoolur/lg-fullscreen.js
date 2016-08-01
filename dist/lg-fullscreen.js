fa1912150a0646e0ca3ed0f89fb72fd9278445d2
sachin <sachi77n@gmail.com>
2016-08-01 09:13:24 +0530

    Dist

--- dist/lg-fullscreen.js
+++ dist/lg-fullscreen.js
@@ -1,5 +1,5 @@
 /**!
- * lg-fullscreen.js | 0.0.1 | July 27th 2016
+ * lg-fullscreen.js | 0.0.1 | August 1st 2016
  * http://sachinchoolur.github.io/lg-fullscreen.js
  * Copyright (c) 2016 Sachin N; 
  * @license Apache 2.0 
